417609bf89263d1e1bb34481fa66859f3bbc9b48
willlockwood <wml248@nyu.edu>
2019-08-31 16:56:19 -0400

    adding classes for binding methods, adapters, converters

---
+++ app/src/main/java/willlockwood/example/stream/utils/BindingAdapters.kt
@@ -0,0 +1,6 @@
+package willlockwood.example.stream.utils
+
+//@BindingAdapter(value = ["app:progressScaled", "android:max"], requireAll = true)
+//fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
+//    progressBar.progress = (likes * max / 5).coerceAtMost(max)
+//}
